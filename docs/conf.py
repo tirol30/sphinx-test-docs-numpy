4c1f0b53fe0123aadfd651d06e29e319a0af66ce
tirol30 <awaya529@yahoo.co.jp>
2024-04-21 13:15:27 +0900

    upp

--- docs/conf.py
+++ docs/conf.py
@@ -17,9 +17,8 @@ release = "1.0"
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
 extensions = [
-    "sphinx.ext.autodoc",  # ソースコード読み込み用
+    "sphinx.ext.autodoc",
     "numpydoc",
 ]
 templates_path = ["_templates"]
