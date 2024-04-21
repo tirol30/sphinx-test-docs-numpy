1cd125bd19e9e53050f73db304bbbc8443c1f0b0
tirol30 <awaya529@yahoo.co.jp>
2024-04-21 13:01:37 +0900

    initial commit

---
+++ docs/conf.py
@@ -0,0 +1,32 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+import os, sys
+
+sys.path.append(os.path.join(os.path.dirname(__file__), "..", "src"))
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "mymodule"
+copyright = "2024, tirol30"
+author = "tirol30"
+release = "1.0"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+extensions = [
+    "sphinx.ext.autodoc",  # ソースコード読み込み用
+    "numpydoc",
+]
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "sphinx_book_theme"
+html_static_path = ["_static"]
---
+++ docs/index.rst
@@ -0,0 +1,22 @@
+.. my-moddd documentation master file, created by
+   sphinx-quickstart on Thu Apr 18 22:35:49 2024.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to my-moddd's documentation!
+====================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   mymodule
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
